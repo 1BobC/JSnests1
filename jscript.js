430a4efe64117997a403f187c584ad09086625ec
1BObC <bob.cocksedge@gmail.com>
2022-07-24 10:10:09 +0100

    eyehunts - how to call an NF in JS Mmm... example doesn't work as shown Can call f1() from outside functions and it returns f2 But cannot equate var d to f1()

--- jscript.js
+++ jscript.js
@@ -1,1 +1,20 @@
 // Essential practise with JS nested functions
+
+//eyehunts - how to call an NF in JS
+function f1() {
+    var c = 0;
+    function f2() {
+        console.log (c++);
+    }
+    return f2()
+}
+//var d = 
+f1();
+//d();
+// d();
+// d();
+// let e = f1();
+// e();
+// e();
+// e();
+
